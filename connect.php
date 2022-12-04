5b9b163cfc32293d727b68f460e2a22ae229178d
ashiba <ashibasama@gmail.com>
2022-12-04 17:41:13 +0900

    next3

--- connect.php
+++ connect.php
@@ -6,6 +6,16 @@ $password = 'root';
 try {
     $pdo = new PDO($dsn, $user, $password);
 
+    $sql = 'CREATE TABLE IF NOT EXISTS users (
+        id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(60) NOT NULL,
+        furigana VARCHAR(60) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        age INT(11),
+        address VARCHAR(255)
+    )';
+    
+    $pdo->query($sql);
     echo '接続成功です！';
 } catch (PDOException $e) {
     exit('接続に失敗しました！' . $e->getMessage());
@@ -14,7 +24,7 @@ try {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
